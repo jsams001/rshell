2c3fa7b176ea973aa607a1c7003ca90f72d932bc
jsams001 <jsams001@ucr.edu>
2017-02-18 04:22:49 +0000

    Erased unused code

--- src/3Or.cpp
+++ src/3Or.cpp
@@ -1,9 +1,9 @@
 #include "1Component.h"
 #include "3Or.h"
 
-Or::Or() {} // default constructor
+Or::Or() {} 
 
-Or::Or(Component* left, Component* right) // constructor with left and right children
+Or::Or(Component* left, Component* right) 
 {
     this->leftChild = left;
     this->rightChild = right;
@@ -28,18 +28,3 @@ bool Or::run()
     }
     return false;
 }
-
-/*void Or::print() {}
-
-void Or::printLeft() 
-{
-    cout << "OR LEFT CHILD: ";
-    this->leftChild->print();
-    cout << endl;
-}
-void Or::printRight() 
-{
-    cout << "OR RIGHT CHILD: ";
-    this->rightChild->print();
-    cout << endl;
-}*/
