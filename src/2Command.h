3bcc03ba7765dbba49573353dd52d7f2eb76e0a9
jsams001 <jsams001@ucr.edu>
2017-03-21 03:55:08 +0000

    Added new function to 2Command.h

--- src/2Command.h
+++ src/2Command.h
@@ -11,12 +11,18 @@ class Command : public Component
     protected: 
         string userCommand;
         vector<char*> flags; // there may be multiple flags per command
+        int inputFD;    // input file descriptor used for redirection
+        int outputFD;   // output file descriptor used for redirection
     
     public:
         Command();
         Command(string userCmd);
         Command(string userCmd, vector<string> userFlags);
         bool run();
+        void setFD(int in, int out);
+        string getUserCommand();
+        int getOutputFD();
+        int getInputFD();
 };
 
 #endif
